83f3c467a8050b7f0b6b750691bbdabae3797027
Felix Oliveira <felixoliveira.dev@gmail.com>
2023-03-29 22:13:51 -0300

    criando componete de video - aula2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 
 import './App.css';
+import Video from './pages/Video'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="app_videos">
+        <Video />
 
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      </div>
     </div>
   );
 }
