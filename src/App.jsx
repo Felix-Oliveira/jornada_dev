3c9ed372f498cdd7ee9928e089681ad174cda34e
Felix Oliveira <felixoliveira.dev@gmail.com>
2023-04-01 15:47:01 -0300

    Criando Sidebar e utilizando props - aula 4

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,29 @@ function App() {
   return (
     <div className="App">
       <div className="app_videos">
-        <Video />
+        {
+
+        }
+        <Video
+          likes={400}
+          messages={200}
+          shares={20}
+          name='Felixxx'
+          description='ddddddddddd'
+          music='musicss'
+          url='https://firebasestorage.googleapis.com/v0/b/jornadadevtr.appspot.com/o/WhatsApp%20Video%202023-03-29%20at%2020.04.52.mp4?alt=media&token=db4be029-27cc-4af9-b75d-3ce5fd1b1fe2'
+        />
+        <Video
+          likes={400}
+          messages={200}
+          shares={20}
+          name='Felixxx'
+          description='ddddddddddd'
+          music='musicss'
+          url='https://firebasestorage.googleapis.com/v0/b/jornadadevtr.appspot.com/o/WhatsApp%20Video%202023-03-29%20at%2020.04.52.mp4?alt=media&token=db4be029-27cc-4af9-b75d-3ce5fd1b1fe2'
+        />
+
+
 
 
       </div>
